8720127fabc3ccf937733896cae9c861ec8a2bf0
root <root@shy-bear-shrinks-fin-02.datacrunch.io>
2025-05-31 18:04:44 +0000

    Huge changes to frontend and flash server :)

--- frontend/client/src/layout/Header.tsx
+++ frontend/client/src/layout/Header.tsx
@@ -1,8 +1,28 @@
 import { CHeader } from "@coreui/react";
 import React from "react";
+import logo from '../angler.svg';
 
 const Header = () => {
-  return <CHeader className="header"></CHeader>;
+  return (
+    <CHeader className="header">
+      <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: '1rem',
+        padding: '1rem'
+      }}>
+        <img src={logo} alt="Logo" style={{ width: '32px', height: '32px' }} />
+        <div style={{
+          fontSize: "1.5rem",
+          fontWeight: "bold",
+          color: "#ffffff"
+        }}>
+          Database Angler
+        </div>
+      </div>
+    </CHeader>
+  );
 };
 
 export default Header;
