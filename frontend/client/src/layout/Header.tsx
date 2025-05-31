e96b0696f677ffb36a7a9c17ef7330684740957d
Maj Nyholm <maj.nyholm@aalto.fi>
2025-05-31 21:26:44 +0300

    cleaning

--- frontend/client/src/layout/Header.tsx
+++ frontend/client/src/layout/Header.tsx
@@ -1,23 +1,27 @@
 import { CHeader } from "@coreui/react";
 import React from "react";
-import logo from '../angler.svg';
+import logo from "../angler.svg";
 
 const Header = () => {
   return (
     <CHeader className="header">
-      <div style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        gap: '1rem',
-        padding: '1rem'
-      }}>
-        <img src={logo} alt="Logo" style={{ width: '32px', height: '32px' }} />
-        <div style={{
-          fontSize: "1.5rem",
-          fontWeight: "bold",
-          color: "#ffffff"
-        }}>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "1rem",
+          padding: "1rem",
+        }}
+      >
+        <img src={logo} alt="Logo" style={{ width: "32px", height: "32px" }} />
+        <div
+          style={{
+            fontSize: "1.5rem",
+            fontWeight: "bold",
+            color: "#ffffff",
+          }}
+        >
           Database Angler
         </div>
       </div>
